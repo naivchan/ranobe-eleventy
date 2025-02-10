1bd801c504c416c041476591c6c1cd5a7907a513
naivchan <40647775+naivchan@users.noreply.github.com>
2025-02-09 21:57:35 -0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -118,4 +118,31 @@ module.exports = function(eleventyConfig) {
   };
 };
 
+ eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().filter(livePosts).forEach(function(item) {
+      if( "tags" in item.data ) {
+        let tags = item.data.tags;
+
+        tags = tags.filter(function(item) {
+
+
+          switch(item) {
+            // this list should match the `filter` list in tags.njk
+            case "all":
+            case "nav":
+            case "post":
+            case "posts":
+              return false;
+          }
+          return true;
+        });
+
+
+        for (const tag of tags) {
+          tagSet.add(tag);
+        }
+      }
+    });
+
 
