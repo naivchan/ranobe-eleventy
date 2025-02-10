955be9a7de856ffa0fb06ab37ed6f9687d6d7eb6
naivchan <40647775+naivchan@users.noreply.github.com>
2025-02-09 22:43:57 -0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -119,5 +119,25 @@ module.exports = function(eleventyConfig) {
   };
 };
 
+eleventyConfig.addCollection("tagList", collection => {
+    const tagsObject = {}
+    collection.getAll().forEach(item => {
+      if (!item.data.tags) return;
+      item.data.tags
+        .filter(tag => !['post', 'all'].includes(tag))
+        .forEach(tag => {
+          if(typeof tagsObject[tag] === 'undefined') {
+            tagsObject[tag] = 1
+          } else {
+            tagsObject[tag] += 1
+          }
+        });
+    });
 
+    const tagList = []
+    Object.keys(tagsObject).forEach(tag => {
+      tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
+    })
+  return tagList.sort((a, b) => b.tagCount - a.tagCount)
+});
 
